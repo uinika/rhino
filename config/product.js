1614d8b9cb60c2b1a0b3a7d7f3633300be0fca60
hank.zheng <uinika@outlook.com>
2018-04-19 16:35:46 +0800

    Update

--- config/product.js
+++ config/product.js
@@ -1,25 +1,28 @@
-const path = require('path'),
-  common = require('./common'),
-  webpack = require('webpack'),
-  webpackMerge = require('webpack-merge'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin'),
-  OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path"),
+  common = require("./common"),
+  webpack = require("webpack"),
+  webpackMerge = require("webpack-merge"),
+  ExtractTextPlugin = require("extract-text-webpack-plugin"),
+  OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const target = 'bundles';
+const target = "bundles";
 
 const extractTextPlugin = new ExtractTextPlugin({
-  filename: path.join(target, '[name].[contenthash].css')
+  filename: path.join(target, "[name].[contenthash].css")
 });
 
 /** product config */
 module.exports = webpackMerge(common, {
   mode: "production",
   output: {
-    path: path.resolve(__dirname, '../build'),
-    filename: path.join(target, '[name].[chunkhash].js'),
-    chunkFilename: path.join(target, '[id].[chunkhash].js')
+    path: path.resolve(__dirname, "../build"),
+    filename: path.join(target, "[name].[chunkhash].js"),
+    chunkFilename: path.join(target, "[id].[chunkhash].js")
   },
-  devtool: 'source-map',
+  devtool: "source-map",
+  optimization: {
+    minimize: true
+  },
   plugins: [
     // new webpack.optimize.UglifyJsPlugin({
     //   compress: {
@@ -28,13 +31,13 @@ module.exports = webpackMerge(common, {
     //   sourceMap: true
     // }),
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: '"production"'
       }
     }),
     extractTextPlugin,
     new OptimizeCssAssetsPlugin({
-      cssProcessor: require('cssnano'),
+      cssProcessor: require("cssnano"),
       cssProcessorOptions: {
         discardComments: {
           removeAll: true
@@ -44,30 +47,37 @@ module.exports = webpackMerge(common, {
     })
   ],
   module: {
-    rules: [{
-      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        name: 'assets/images/[name].[hash].[ext]'
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "assets/images/[name].[hash].[ext]"
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "assets/fonts/[name].[hash].[ext]"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractTextPlugin.extract({
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ],
+          fallback: "style-loader"
+        })
       }
-    }, {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        name: 'assets/fonts/[name].[hash].[ext]'
-      }
-    }, {
-      test: /\.scss$/,
-      use: extractTextPlugin.extract({
-        use: [{
-          loader: 'css-loader',
-        }, {
-          loader: 'sass-loader'
-        }],
-        fallback: 'style-loader'
-      })
-    }]
+    ]
   }
 });
