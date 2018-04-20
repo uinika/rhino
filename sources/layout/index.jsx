926d795701d0efc71615ae58ebfc0a13f38ab3ca
hank.zheng <uinika@outlook.com>
2018-04-20 18:09:57 +0800

    Update

--- sources/layout/index.jsx
+++ sources/layout/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.scss";
 import { Route, Link, Switch } from "react-router-dom";
 import Loadable from "react-loadable";
+import Loading from "../common/loading.jsx";
 
 export default class Layout extends React.Component {
   render() {
