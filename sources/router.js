3c641722315b1a86af7bf2177715f3eec598b4b3
hank.zheng <uinika@outlook.com>
2018-04-20 17:40:39 +0800

    Update

--- sources/router.js
+++ sources/router.js
@@ -1,13 +1,28 @@
 import { Route, Link, Switch } from "react-router-dom";
 import React from "react";
-import Login from "./login/index.jsx";
 import Layout from "./layout/index.jsx";
+import Loadable from "react-loadable";
+import Loading from "./common/loading.jsx";
+import "antd/dist/antd.css";
 
 export default () => (
   <div>
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/login" component={Login} />
+      <Route
+        exact
+        path="/"
+        component={Loadable({
+          loader: () => import("./login/index.jsx"),
+          loading: Loading
+        })}
+      />
+      <Route
+        path="/login"
+        component={Loadable({
+          loader: () => import("./login/index.jsx"),
+          loading: Loading
+        })}
+      />
       <Route path="/layout" component={Layout} />
     </Switch>
   </div>
