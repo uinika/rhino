b6e2be3879b82bd618ea4f354accefc9368e9898
hank.zheng <uinika@outlook.com>
2018-04-20 15:05:18 +0800

    Update

--- sources/layout/index.jsx
+++ sources/layout/index.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./style.scss";
+import Dashboard from "../dashboard/index.jsx";
+import { Route, Link, Switch } from "react-router-dom";
 
 export default class Layout extends React.Component {
   render() {
-    return <h1>Hello, Layout !</h1>;
+    return (
+      <div>
+        <nav>导航</nav>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    );
   }
 }
--- sources/router.js
+++ sources/router.js
@@ -2,15 +2,13 @@ import { Route, Link, Switch } from "react-router-dom";
 import React from "react";
 import Login from "./login/index.jsx";
 import Layout from "./layout/index.jsx";
-import Dashboard from "./dashboard/index.jsx";
 
 export default () => (
   <div>
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/login" component={Login} />
-      <Route exact path="/layout" component={Layout} />
-      <Route path="/layout/dashboard" component={Dashboard} />
+      <Route path="/layout" component={Layout} />
     </Switch>
   </div>
 );
