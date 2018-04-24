a68d5caa6ac907d15dd0e1b30c0ef170a5e810a2
hank.zheng <uinika@outlook.com>
2018-04-24 11:39:08 +0800

    Update

--- sources/layout/index.jsx
+++ sources/layout/index.jsx
@@ -3,51 +3,64 @@ import "./style.scss";
 import { Route, Link, Switch } from "react-router-dom";
 import Loadable from "react-loadable";
 import Loading from "../common/loading";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
 
-const { Header, Content, Sider } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
 
 export default class GlobalLayout extends React.Component {
+  state = {
+    collapsed: false
+  };
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
   render() {
     return (
-      <Layout id="layout">
-        <Header>
-          <h1 className="logo">虚拟车钥匙管理系统</h1>
-          <Menu
-            className="menu"
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["1"]}
-          >
-            <Menu.Item key="1">
+      <Layout id="layout" style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo">Admin</div>
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="0">
               <Link to="/layout/dashboard">
-                <i className="fa fa-align-left" />&nbsp;&nbsp; 首页
+                <Icon type="appstore" />
+                <span>Dashboard</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="1">
+              <Link to="/layout/key">
+                <Icon type="key" />
+                <span>Key</span>
               </Link>
             </Menu.Item>
             <Menu.Item key="2">
-              <Link to="/layout/key">
-                <i className="fa fa-key" />&nbsp;&nbsp;钥匙管理
+              <Link to="/layout/vehicle">
+                <Icon type="car" />
+                <span>Vehicle</span>
               </Link>
             </Menu.Item>
             <Menu.Item key="3">
-              <Link to="/layout/vehicle">
-                <i className="fa fa-car" />&nbsp;&nbsp;车辆管理
+              <Link to="/layout/user">
+                <Icon type="user" />
+                <span>User</span>
               </Link>
             </Menu.Item>
             <Menu.Item key="4">
-              <Link to="/layout/user">
-                <i className="fa fa-users" />&nbsp;&nbsp;用户管理
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="5">
               <Link to="/layout/setting">
-                <i className="fa fa-cogs" />&nbsp;&nbsp;系统设置
+                <Icon type="setting" />
+                <span>Setting</span>
               </Link>
             </Menu.Item>
           </Menu>
-        </Header>
+        </Sider>
         <Layout>
-          <Content>
+          <Header style={{ background: "#fff", padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
             <Switch>
               <Route
                 path="/layout/dashboard"
@@ -86,6 +99,9 @@ export default class GlobalLayout extends React.Component {
               />
             </Switch>
           </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Rhino ©2018 Created by Hank
+          </Footer>
         </Layout>
       </Layout>
     );
