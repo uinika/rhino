94522cdc5432da79ea2f11b9dcbc78ade135fe52
hank.zheng <uinika@outlook.com>
2018-04-27 11:26:56 +0800

    Update

--- sources/user/index.jsx
+++ sources/user/index.jsx
@@ -1,60 +1,147 @@
 import React from "react";
 import "./style.scss";
-import { Table } from "antd";
-
+import { Row, Col, Form, Icon, Input, Button } from "antd";
+import { Table, Popconfirm } from "antd";
 import CommonSearchForm from "../common/components/search-form";
 
-const columns = [
-  {
-    title: "Full Name",
-    width: 100,
-    dataIndex: "name",
-    key: "name",
-    fixed: "left"
-  },
-  { title: "Name", width: 200, dataIndex: "age", key: "age", fixed: "left" },
-  { title: "ID", dataIndex: "address", key: "1", width: 150 },
-  { title: "Date", dataIndex: "address", key: "2", width: 150 },
-  { title: "Column 3", dataIndex: "address", key: "3", width: 150 },
-  { title: "Column 4", dataIndex: "address", key: "4", width: 150 },
-  { title: "Column 5", dataIndex: "address", key: "5", width: 150 },
-  { title: "User Info", dataIndex: "address", key: "6", width: 150 },
-  { title: "User Info", dataIndex: "address", key: "7", width: 150 },
-  { title: "User Info", dataIndex: "address", key: "8" },
-  {
-    title: "Operation",
-    key: "operation",
-    fixed: "right",
-    width: 100,
-    render: () => <a href="javascript:;">More</a>
-  }
-];
-
 const data = [];
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 100; i++) {
   data.push({
-    key: i,
-    name: `Userame`,
-    age: 417324890174010,
-    address: `Some User Infomation ${i}`
+    key: i.toString(),
+    name: `Edrward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`
   });
 }
 
-export default class key extends React.Component {
-  render() {
-    return (
-      <div id="key">
-        <div className="search-form common-box">
-          <CommonSearchForm />
+const EditableCell = ({ editable, value, onChange }) => (
+  <div>
+    {editable ? (
+      <Input
+        style={{ margin: "-5px 0" }}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      />
+    ) : (
+      value
+    )}
+  </div>
+);
+
+const FormItem = Form.Item;
+
+export default Form.create()(
+  class key extends React.Component {
+    constructor(props) {
+      super(props);
+      this.columns = [
+        {
+          title: "name",
+          dataIndex: "name",
+          width: "25%",
+          render: (text, record) => this.renderColumns(text, record, "name")
+        },
+        {
+          title: "age",
+          dataIndex: "age",
+          width: "15%",
+          render: (text, record) => this.renderColumns(text, record, "age")
+        },
+        {
+          title: "address",
+          dataIndex: "address",
+          width: "40%",
+          render: (text, record) => this.renderColumns(text, record, "address")
+        },
+        {
+          title: "operation",
+          dataIndex: "operation",
+          render: (text, record) => {
+            const { editable } = record;
+            return (
+              <div className="editable-row-operations">
+                {editable ? (
+                  <span>
+                    <a onClick={() => this.save(record.key)}>Save</a>
+                    <Popconfirm
+                      title="Sure to cancel?"
+                      onConfirm={() => this.cancel(record.key)}
+                    >
+                      <a>Cancel</a>
+                    </Popconfirm>
+                  </span>
+                ) : (
+                  <a onClick={() => this.edit(record.key)}>Edit</a>
+                )}
+              </div>
+            );
+          }
+        }
+      ];
+      this.state = { data };
+      this.cacheData = data.map(item => ({ ...item }));
+    }
+    renderColumns(text, record, column) {
+      return (
+        <EditableCell
+          editable={record.editable}
+          value={text}
+          onChange={value => this.handleChange(value, record.key, column)}
+        />
+      );
+    }
+    handleChange(value, key, column) {
+      const newData = [...this.state.data];
+      const target = newData.filter(item => key === item.key)[0];
+      if (target) {
+        target[column] = value;
+        this.setState({ data: newData });
+      }
+    }
+    edit(key) {
+      const newData = [...this.state.data];
+      const target = newData.filter(item => key === item.key)[0];
+      if (target) {
+        target.editable = true;
+        this.setState({ data: newData });
+      }
+    }
+    save(key) {
+      const newData = [...this.state.data];
+      const target = newData.filter(item => key === item.key)[0];
+      if (target) {
+        delete target.editable;
+        this.setState({ data: newData });
+        this.cacheData = newData.map(item => ({ ...item }));
+      }
+    }
+    cancel(key) {
+      const newData = [...this.state.data];
+      const target = newData.filter(item => key === item.key)[0];
+      if (target) {
+        Object.assign(
+          target,
+          this.cacheData.filter(item => key === item.key)[0]
+        );
+        delete target.editable;
+        this.setState({ data: newData });
+      }
+    }
+    render() {
+      return (
+        <div id="key">
+          <div className="search-form common-box">
+            <CommonSearchForm />
+          </div>
+          <div className="result-table common-box">
+            <Table
+              bordered
+              dataSource={this.state.data}
+              columns={this.columns}
+            />
+          </div>
         </div>
-        <div className="result-table common-box">
-          <Table
-            columns={columns}
-            dataSource={data}
-            scroll={{ x: 2500, y: 300 }}
-          />
-        </div>
-      </div>
-    );
+      );
+    }
   }
-}
+);
--- sources/vehicle/index.jsx
+++ sources/vehicle/index.jsx
@@ -1,24 +1,60 @@
 import React from "react";
 import "./style.scss";
-import { Row, Col, Form, Icon, Input, Button } from "antd";
+import { Table } from "antd";
 
 import CommonSearchForm from "../common/components/search-form";
 
-const FormItem = Form.Item;
-
-export default Form.create()(
-  class key extends React.Component {
-    render() {
-      return (
-        <div id="key">
-          <div className="search-form common-box">
-            <CommonSearchForm />
-          </div>
-          <div className="result-table common-box">
-
-          </div>
-        </div>
-      );
-    }
+const columns = [
+  {
+    title: "Vehicle Model",
+    width: 150,
+    dataIndex: "name",
+    key: "name",
+    fixed: "left"
+  },
+  { title: "VIN", width: 200, dataIndex: "age", key: "age", fixed: "left" },
+  { title: "Date of Register", dataIndex: "address", key: "1", width: 150 },
+  { title: "Date of Purchase", dataIndex: "address", key: "2", width: 150 },
+  { title: "Column 3", dataIndex: "address", key: "3", width: 150 },
+  { title: "Column 4", dataIndex: "address", key: "4", width: 150 },
+  { title: "Column 5", dataIndex: "address", key: "5", width: 150 },
+  { title: "Vehicle Info", dataIndex: "address", key: "6", width: 150 },
+  { title: "Vehicle Info", dataIndex: "address", key: "7", width: 150 },
+  { title: "Vehicle Info", dataIndex: "address", key: "8" },
+  {
+    title: "Operation",
+    key: "operation",
+    fixed: "right",
+    width: 100,
+    render: () => <a href="javascript:;">More</a>
   }
-);
+];
+
+const data = [];
+for (let i = 0; i < 1000; i++) {
+  data.push({
+    key: i,
+    name: `Mondeo CX30`,
+    age: 417324890174010,
+    address: `Some Vehicle Infomation ${i}`
+  });
+}
+
+export default class key extends React.Component {
+  render() {
+    return (
+      <div id="key">
+        <div className="search-form common-box">
+          <CommonSearchForm />
+        </div>
+        <div className="result-table common-box">
+          <Table
+            columns={columns}
+            dataSource={data}
+            scroll={{ x: 2500, y: 300 }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
