13015d5827b853350444e38e607f6d05de7dbb84
hank.zheng <uinika@outlook.com>
2018-05-07 18:10:55 +0800

    Update

--- sources/app.js
+++ sources/app.js
@@ -5,9 +5,9 @@ import { hot } from "react-hot-loader";
 import Router from "./router.js";
 import "babel-polyfill";
 // css
-import "./common/styles/awesome/css/fontawesome-all.min.css";
 import "./common/styles/base.scss";
 import "./common/styles/reset.scss";
+import "./common/styles/awesome/css/fontawesome-all.min.css";
 // theme
 import "antd/dist/antd.less";
 import "./common/styles/theme.less";
