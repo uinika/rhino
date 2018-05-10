b36aadf98068df4016e1307fc9293857b3e9818f
hank.zheng <uinika@outlook.com>
2018-05-10 11:56:56 +0800

    Update

--- sources/layout/index.jsx
+++ sources/layout/index.jsx
@@ -20,7 +20,7 @@ export default class GlobalLayout extends React.Component {
   render() {
     return (
       <Layout id="layout" className="animated fadeIn">
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+        <Sider className="left" trigger={null} collapsible collapsed={this.state.collapsed}>
           <div className="logo">Admin</div>
           <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
             <Menu.Item key="1">
@@ -55,22 +55,15 @@ export default class GlobalLayout extends React.Component {
             </Menu.Item>
           </Menu>
         </Sider>
-        <Layout>
-          <Header style={{ background: "#fff", padding: 0 }}>
+        <Layout className="right">
+          <Header className="header">
             <Icon
               className="trigger"
               type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
               onClick={this.toggle}
             />
           </Header>
-          <Content
-            style={{
-              margin: "24px 16px",
-              padding: 24,
-              background: "#fff",
-              minHeight: 280
-            }}
-          >
+          <Content className="content">
             <Switch>
               <Route
                 path="/layout/dashboard"
