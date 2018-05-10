f29f1995fc954a91e2fe64da78ebd2636f043802
hank.zheng <uinika@outlook.com>
2018-05-10 18:04:42 +0800

    Update

--- sources/agreement/index.jsx
+++ sources/agreement/index.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import "./style.scss";
+import { Breadcrumb, Card, Input, Select, Col, Row } from "antd";
 
 export default class Agreement extends React.Component {
   render() {
     return (
       <div id="agreement" className="animated fadeIn">
-        <h1>Agreement</h1>
+        <Row gutter={1}>
+          <Col span={24}>
+            <Card bordered={true}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={1}>
+          <Col span={24}>
+            <Card bordered={false}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+        </Row>
       </div>
-    )
+    );
   }
 }
--- sources/dashboard/index.jsx
+++ sources/dashboard/index.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 import "./style.scss";
+import { Breadcrumb, Card, Input, Select, Col, Row } from "antd";
+const Search = Input.Search;
+const InputGroup = Input.Group;
+const Option = Select.Option;
 
 export default class Dashboard extends React.Component {
   render() {
     return (
       <div id="dashboard" className="animated fadeIn">
-        <h1>Dashboard</h1>
+        <Card>
+          <Breadcrumb>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <a href="#/layout/dashboard">Dashboard</a>
+            </Breadcrumb.Item>
+          </Breadcrumb>
+        </Card>
+        <Row gutter={1}>
+          <Col span={12}>
+            <Card
+              title="Status"
+              bordered={false}
+              extra={<a href="#">More</a>}
+            >
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card
+              title="History"
+              bordered={false}
+              extra={<a href="#">More</a>}
+            >
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+        </Row>
       </div>
-    )
+    );
   }
 }
