5a25047344f1440dd7b09d2412554a96ac6a11b6
hank.zheng <uinika@outlook.com>
2018-05-11 12:48:22 +0800

    Update

--- sources/dashboard/index.jsx
+++ sources/dashboard/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.scss";
-import { Breadcrumb, Card, Input, Select, Col, Row } from "antd";
+import { Breadcrumb, Card, Input, Select, Col, Row, Icon } from "antd";
 const Search = Input.Search;
 const InputGroup = Input.Group;
 const Option = Select.Option;
@@ -9,15 +9,17 @@ export default class Dashboard extends React.Component {
   render() {
     return (
       <div id="dashboard" className="animated fadeIn">
-        <Card>
-          Charts
-        </Card>
+        <Card>Charts</Card>
         <Row gutter={1}>
           <Col span={12}>
             <Card
-              title="Status"
+              title={
+                <span>
+                  <Icon type="key" />&nbsp;&nbsp;Key
+                </span>
+              }
               bordered={false}
-              extra={<a href="#">More</a>}
+              extra={<a href="#/layout/key">More</a>}
             >
               <p>Card content</p>
               <p>Card content</p>
@@ -26,9 +28,43 @@ export default class Dashboard extends React.Component {
           </Col>
           <Col span={12}>
             <Card
-              title="History"
+              title={
+                <span>
+                  <Icon type="car" />&nbsp;&nbsp;Vehicle
+                </span>
+              }
               bordered={false}
-              extra={<a href="#">More</a>}
+              extra={<a href="#/layout/vehicle">More</a>}
+            >
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card
+              title={
+                <span>
+                  <Icon type="user" />&nbsp;&nbsp;User
+                </span>
+              }
+              bordered={true}
+              extra={<a href="#/layout/user">More</a>}
+            >
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card
+              title={
+                <span>
+                  <Icon type="book" />&nbsp;&nbsp;Agreement
+                </span>
+              }
+              bordered={true}
+              extra={<a href="#/layout/agreement/bluetooth">More</a>}
             >
               <p>Card content</p>
               <p>Card content</p>
